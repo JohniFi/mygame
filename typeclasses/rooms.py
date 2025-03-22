d7b2a9ff0f94a481ccd0beaed83f40d3379a24ef
JohniFi <25084862+JohniFi@users.noreply.github.com>
2025-03-22 22:23:13 +0100

    update room get_display_name

--- typeclasses/rooms.py
+++ typeclasses/rooms.py
@@ -21,4 +21,21 @@ class Room(ObjectParent, DefaultRoom):
     properties and methods available on all Objects.
     """
 
-    pass
+
+    def get_display_name(self, looker=None, **kwargs):
+        """
+        Displays the name of the object in a viewer-aware manner.
+
+        Args:
+            looker (DefaultObject): The object or account that is looking at or getting information
+                for this object.
+
+        Returns:
+            str: A name to display for this object. By default this returns the `.name` of the object.
+
+        Notes:
+            This function can be extended to change how object names appear to users in character,
+            but it does not change an object's keys or aliases when searching.
+
+        """
+        return f"|315{self.name}|n"
